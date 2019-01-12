fd26a83555fbb56807acb509ba58389f54b54ab0
Jason Kirshner <jasonkirshner13@gmail.com>
2019-01-11 17:27:42 -0800

    Adding ESLint package

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
 
 // Set up the express app
-const app = express();
+const app = express()
 
 // Log requests to the console.
-app.use(logger('dev'));
+app.use(logger('dev'))
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+    res.header("Access-Control-Allow-Origin", '*')
+    res.header("Access-Control-Allow-Credentials", true)
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+    next()
+})
 
 // Require our routes into the application.
-require('./server/routes')(app);
+require('./server/routes')(app)
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
-}));
+    message: 'Unable to access the Vbay API. Check the endpoint for typos.',
+}))
 
-module.exports = app;
+module.exports = app
