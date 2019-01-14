5b8106d50105433a2662ebe9518ff0676af2871a
Jason Kirshner <jasonkirshner13@gmail.com>
2019-01-13 16:21:30 -0800

    Created routes for new models

--- test/test-server.js
+++ test/test-server.js
@@ -1,10 +1,10 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../app');
+var chai = require('chai')
+var chaiHttp = require('chai-http')
+var server = require('../app')
 var User = require('../server/models/user')
-var should = chai.should();
+var should = chai.should()
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
 describe('Users', function () {
     it('should list ALL users on /users GET', function (done) {
@@ -23,37 +23,37 @@ describe('Users', function () {
                 res.should.have.status(200)
                 res.should.be.json
                 res.body.should.be.a('object')
-                res.body.should.have.property('id');
-                res.body.should.have.property('userid');
-                res.body.should.have.property('email');
-                res.body.should.have.property('password');
-                res.body.should.have.property('fname');
-                res.body.should.have.property('lname');
-                res.body.userid.should.equal('hello');
-                res.body.email.should.equal('dsa');
-                res.body.password.should.equal('ads');
-                res.body.fname.should.equal('dsa');
-                res.body.lname.should.equal('dsads');
-                res.body.id.should.equal(3);
+                res.body.should.have.property('id')
+                res.body.should.have.property('id')
+                res.body.should.have.property('email')
+                res.body.should.have.property('password')
+                res.body.should.have.property('fname')
+                res.body.should.have.property('lname')
+                res.body.id.should.equal('hello')
+                res.body.email.should.equal('dsa')
+                res.body.password.should.equal('ads')
+                res.body.fname.should.equal('dsa')
+                res.body.lname.should.equal('dsads')
+                res.body.id.should.equal(3)
                 done()
             })
     })
     it('should add a SINGLE user on /users POST', function (done) {
         chai.request(server)
             .post('/api/users')
-            .send({ 'userid': 'man21', 'email': 'pdoap@dksma.com', 'password': 'dsaui82', 'fname': '29102waido', 'lname': 'dsad334fed' })
+            .send({ 'id': 'man21', 'email': 'pdoap@dksma.com', 'password': 'dsaui82', 'fname': '29102waido', 'lname': 'dsad334fed' })
             .end(function (err, res) {
                 res.should.have.status(201)
                 res.should.be.json
                 res.body.should.be.a('object')
-                res.body.should.have.property('id');
-                res.body.should.have.property('userid');
-                res.body.should.have.property('email');
-                res.body.should.have.property('password');
-                res.body.should.have.property('fname');
-                res.body.should.have.property('lname');
-                done();
-            });
+                res.body.should.have.property('id')
+                res.body.should.have.property('id')
+                res.body.should.have.property('email')
+                res.body.should.have.property('password')
+                res.body.should.have.property('fname')
+                res.body.should.have.property('lname')
+                done()
+            })
     })
     it('should update a SINGLE user on /user/:id PUT', function (done) {
         chai.request(server)
@@ -61,19 +61,19 @@ describe('Users', function () {
             .end(function (err, res) {
                 chai.request(server)
                     .put('/users/' + res.id)
-                    .send({ 'userid': 'dsas' })
+                    .send({ 'id': 'dsas' })
                     .end(function (error, response) {
                         res.should.have.status(200)
                         res.should.be.json
                         res.body.should.be.a('object')
-                        res.body.should.have.property('id');
-                        res.body.should.have.property('userid');
-                        res.body.should.have.property('email');
-                        res.body.should.have.property('password');
-                        res.body.should.have.property('fname');
-                        res.body.should.have.property('lname');
-                        done();
-                    });
+                        res.body.should.have.property('id')
+                        res.body.should.have.property('id')
+                        res.body.should.have.property('email')
+                        res.body.should.have.property('password')
+                        res.body.should.have.property('fname')
+                        res.body.should.have.property('lname')
+                        done()
+                    })
             })
     })
-});
+})
